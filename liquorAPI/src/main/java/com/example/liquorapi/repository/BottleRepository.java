a4c8b729bd4802c43cec2e9c9dc8288a2ccef637
Brian <brian.1980.habeeb@gmail.com>
2022-02-24 10:38:53 -0500

    distributor repository interface created

---
+++ liquorAPI/src/main/java/com/example/liquorapi/repository/BottleRepository.java
@@ -0,0 +1,4 @@
+package com.example.liquorapi.repository;
+
+public interface BottleRepository {
+}
---
+++ liquorAPI/src/main/java/com/example/liquorapi/repository/DistributorRepository.java
@@ -0,0 +1,7 @@
+package com.example.liquorapi.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.liquorapi.model.Distributor
+
+public interface DistributorRepository extends JpaRepository<Distributor, Long> {
+    Distributor findByName(String distributorName);
+}
