ab02152a64b7e9b76c4fa474269857d36850a6a4
Brian <brian.1980.habeeb@gmail.com>
2022-02-24 10:44:20 -0500

    bottle repository added

--- liquorAPI/src/main/java/com/example/liquorapi/repository/BottleRepository.java
+++ liquorAPI/src/main/java/com/example/liquorapi/repository/BottleRepository.java
@@ -1,4 +1,9 @@
 package com.example.liquorapi.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.liquorapi.model.Bottle;
+import java.util.List;
 
-public interface BottleRepository {
+public interface BottleRepository extends JpaRepository<Bottle, Long> {
+    Bottle findByName(String bottleName);
 }
--- liquorAPI/src/main/java/com/example/liquorapi/repository/DistributorRepository.java
+++ liquorAPI/src/main/java/com/example/liquorapi/repository/DistributorRepository.java
@@ -1,6 +1,6 @@
 package com.example.liquorapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.liquorapi.model.Distributor
+import com.example.liquorapi.model.Distributor;
 
 public interface DistributorRepository extends JpaRepository<Distributor, Long> {
     Distributor findByName(String distributorName);
